0e6f64e528d1a4e18ae94cf1f13342aff997b525
Gulshid Zada <gulshidzada4@gmail.com>
2025-12-24 16:39:02 +0500

    Python Course start for tik tok

---
+++ python_Basics/Porgram/content.py
@@ -0,0 +1,14 @@
+          #    *** Chapter 1. Basics Fundamental ***
+            # 1. introduction to Python
+            # 2. Installation of Python
+            # 3. Editors in Python
+            # 4. print and input Statement in Python
+            # 5. Variable and Constant in Python
+            # 6. Comments in Python
+            # 7. DataTypes in Python
+            # 8. Error and Types of Error in Python
+            # 9. Operator and Types of Operator in Python
+            # 10. Increment and Decrement in Python
+            # 11. Multiple way to Define Incre / Decre
+            # 12. Type Casting in Python
+            # 13. Escape Sequences , different way to display the value in print, in one how display two print (end method), identation

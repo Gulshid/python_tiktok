ec6471a88ba3ca192434ba751926668afa777ed2
Gulshid Zada <gulshidzada4@gmail.com>
2025-12-28 17:16:38 +0500

    datatype start

---
+++ python_Basics/Program/datatype.py
@@ -0,0 +1,31 @@
+# Data type in Python:
+#    A data type tells Python what kind 
+#    of value a variable holds and what
+#    operations can be performed on it.
+
+# Examples:
+
+# 1. int → whole numbers (e.g., 5)
+
+# 2. float → decimal numbers (e.g., 3.14)
+
+# 3. str → text (e.g., "Hello")
+
+# 4. bool → true or false (True, False)
+
+# 5. List:
+# A collection of items that is ordered and changeable.
+# Example: [1, 2, 3]
+
+# 6. Tuple:
+# A collection of items that is ordered but unchangeable.
+# Example: (1, 2, 3)
+
+# 7. Set:
+# A collection of unique items that is unordered.
+# Example: {1, 2, 3}
+
+# 8. Dictionary (dict):
+# A collection of key–value pairs used to store data.
+# Example: {"name": "Ali", "age": 20}
+

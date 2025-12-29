9cd46ce2ee653c42d73f85771ec1d614b4959c15
Gulshid Zada <gulshidzada4@gmail.com>
2025-12-29 11:28:44 +0500

    datatype explanation done

--- python_Basics/Program/datatype.py
+++ python_Basics/Program/datatype.py
@@ -6,12 +6,18 @@
 # Examples:
 
 # 1. int → whole numbers (e.g., 5)
+number = 5
 
 # 2. float → decimal numbers (e.g., 3.14)
+num = 3.12
 
 # 3. str → text (e.g., "Hello")
+text = 'Hello'
+text_2 = "WOrld"
 
 # 4. bool → true or false (True, False)
+istrue = True
+isfalse = False
 
 # 5. List:
 # A collection of items that is ordered and changeable.

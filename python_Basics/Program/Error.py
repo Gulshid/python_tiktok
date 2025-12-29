d3aeeb8cb6495963ce10946d0efbfd782726dc11
Gulshid Zada <gulshidzada4@gmail.com>
2025-12-29 12:24:39 +0500

    Error and types of error complete

---
+++ python_Basics/Program/Error.py
@@ -0,0 +1,18 @@
+# 8. Error and Types of Error in Python
+# Error and Types of Error
+# Error is a mistake in a program that stops it from running correctly.
+
+# 1. Syntax Error:
+# writing rules are broken
+
+print("Hello")
+# print " Hello "
+
+# 2. Semantic Error:
+# program runs but gives wrong result
+length = 2, width = 3
+area = length * length # Incorrect
+area = length * width # correct 
+
+# 3. Runtime Error:
+# error while program runs

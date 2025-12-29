8d7d77264b06b2e2f86e3b296f398cec33beb8ca
Gulshid Zada <gulshidzada4@gmail.com>
2025-12-29 11:46:34 +0500

    Datatype done

---
+++ python_Basics/Program/datatype_practical.py
@@ -0,0 +1,44 @@
+# Datatype in C++
+
+
+# int {numeric , eg:> 2,3,4...}
+a = 4
+
+# float {numeric , eg:> 2.3, 4.5, 6.3}
+b = 2.3
+
+# string {collection of words}
+c = 'Hello this is string datatype'
+d = "Hello WOrld"
+
+# bool {True, False}
+e = True
+f = False
+
+# List {[1,2,3, 3.4, "Hello", True]}
+g = [1,2,3, 2.3, "Hello", True]
+
+# Tuple {(1,2,3, "Hello", False, 4.3)}
+h = (1,2,3,"Hello", False, 3.4)
+
+# Set {{1,2,3}}
+i = {1,2,3,4,5}
+
+
+# dict {key - value pairs}
+z = {
+    "name" : "Gulshid Zada",
+    "roll" : 391
+}
+
+print("**** Datatype in python ****")
+print("int datatype :", a)
+print("float datatype :", b)
+print("string datatype :", c)
+print("string datatype :", d)
+print("bool datatype :", e)
+print("bool datatype :", f)
+print("List datatype :", g)
+print("Tuple datatype :", h)
+print("Set datatype :", i)
+print("Dict datatype :", z)

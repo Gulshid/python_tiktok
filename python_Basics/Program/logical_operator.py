30a3c33be59c18efa4f68d46a9a55fbcae9c9a65
Gulshid Zada <gulshidzada4@gmail.com>
2025-12-30 14:06:42 +0500

    logical operator done

---
+++ python_Basics/Program/logical_operator.py
@@ -0,0 +1,13 @@
+# 3. Logical Operator ===> and , or , not 
+a = 5
+b = 4
+c = 2
+
+# and operator
+print("and Operator :",  a > b and a > c) # True 
+
+# or operator 
+print("or Operator :", a > b or a < c) # True
+
+# not operator 
+print("Not Operator :", not a > b) # True ==> not False

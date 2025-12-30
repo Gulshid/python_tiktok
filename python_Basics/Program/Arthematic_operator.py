82a2a356c578b428915381fec2f90d39a8b71d2d
Gulshid Zada <gulshidzada4@gmail.com>
2025-12-30 13:53:00 +0500

    arthematic and relation operator done

--- python_Basics/Program/Arthematic_operator.py
+++ python_Basics/Program/Arthematic_operator.py
@@ -1,1 +1,22 @@
-# 1. Arthematic Operator
+# 1. Arthematic Operator ===> + , -, *, /, %
+print("*** Arthematic Operators ***")
+# x = 3
+# y = 6
+
+x = float(input('Enter the value for x :'))
+y = float(input('Enter the value for y :'))
+
+# add +
+print("the additioin of two value is :", x + y)
+
+# sub -
+print("The sub of two value is :", x - y)
+
+# mul *
+print("The mul of two value is :", x * y)
+
+# divide /
+print("The division of two value is :", x / y)
+
+# modulas %
+print("The modulas of two value is :", x % y)
---
+++ python_Basics/Program/Relational_operator.py
@@ -0,0 +1,21 @@
+# 2. Relational Operator ===> >, <, >=, <=, != , ==
+a = 4
+b = 8
+
+# greater >
+print("> operator :",  a > b) # False
+
+# less <
+print("< operator :", a < b) # True
+
+# greater than or equal to >=
+print(">= operator :", a >= b) # False
+
+# less than or equal to <=
+print("<= operator :", a <= b) # True
+
+# not equal to !=
+print("!= operator :", a != b) # True
+
+# equal equal to ==
+print("== operator :", a == b) # False

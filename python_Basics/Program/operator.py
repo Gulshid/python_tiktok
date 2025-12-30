94b20042458bf9be86fbad048d3dfb42dd1f49e0
Gulshid Zada <gulshidzada4@gmail.com>
2025-12-30 10:41:53 +0500

    OperTor start

---
+++ python_Basics/Program/operator.py
@@ -0,0 +1,14 @@
+# 9. Operator and Types of Operator in Python
+
+# Operator and Its Types
+# Operator and Operand
+
+# Example 1 --> x + y ==> x and y = Operand , + ===> Operator
+# Example 2 --> 2 + 4 ===> 2 and 4 = Operand , + ===> Operator
+
+# Types of Operator 
+# 1. Arthematic Operator ===> + , -, *, /, %
+# 2. Relational Operator ===> >, <, >=, <=, != , ==
+# 3. Logical Operator ===> and , or , not 
+# 4. Bitwise Operator ===> & , | , ~ , ^
+# 5. Arthematic and Assignement Operator ===> +=, -=, *=, /=, %=, =

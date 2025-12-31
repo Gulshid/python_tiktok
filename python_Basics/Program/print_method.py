cfc474921aeb89a23199c9a153af0a9fc0f382a5
Gulshid Zada <gulshidzada4@gmail.com>
2025-12-31 10:10:54 +0500

    multiple way to display

---
+++ python_Basics/Program/print_method.py
@@ -0,0 +1,9 @@
+# Multiple way to display a value in print
+a = 2
+b = 4
+
+# Method 1 
+print("the value of a :", a, ", the value of b :", b)
+
+# Method 2
+print(f"The value of a :{a} and the value of b :{b}")

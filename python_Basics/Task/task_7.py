d493e763809cada7f8da1ee114a9d0907a335931
Gulshid Zada <gulshidzada4@gmail.com>
2026-01-11 11:15:56 +0500

    task 7 and 8 done

---
+++ python_Basics/Task/task_7.py
@@ -0,0 +1,41 @@
+# 7. Write a Program that can print all Datatype value
+# integer
+a = 3
+
+# float
+b = 3.14
+
+# string
+c = "This is Python Programming"
+
+# boolean
+d = True
+e = False
+
+# List 
+my_list = [1, 2.3, "Text", True]
+
+# Tuple
+my_tuple = (2, 2,5, "value", False)
+
+# Set 
+my_set = {1,2,3,4,5}
+
+# Dict 
+my_dict = {
+    "name": "Gulshid Zada",
+    'Roll No': 391,
+    'Marks': 500
+}
+
+# Print all the data types 
+print("*** All Data types values ***")
+print("Integer :",a)
+print("Float :", b)
+print("String :",c)
+print("Bool :", d)
+print("Bool :", e)
+print("List :", my_list)
+print("Tuple :", my_tuple)
+print("Set :", my_set)
+print("DIct :", my_dict)
---
+++ python_Basics/Task/task_8.py
@@ -0,0 +1,6 @@
+# 8. Write a Program that can input name and Age of Person and then display
+name = str(input("Enter your Name :"))
+age = int(input("Enter your Age :"))
+
+print("The Name is :", name, " and the Age is :", age )
+print(f"The Person Name is : {name} and the Age is : {age}")

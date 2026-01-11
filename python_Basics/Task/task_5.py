2ef061d7b123a55b7bf153ffa27a6a7984f1f0fc
Gulshid Zada <gulshidzada4@gmail.com>
2026-01-11 10:49:21 +0500

    Task 5 and 6 done

---
+++ python_Basics/Task/task_5.py
@@ -0,0 +1,16 @@
+# 5. How to Divide Two Number with and Without Third Variable
+# Method 1
+a = 10
+b = 2
+
+# Method 2
+# w = float(input("Enter the value for w :"))
+# r = float(input("Enter the value for r :"))
+
+
+# Without thired varible method 
+print("The division of two value is :", a / b)
+
+# With thired variable method 
+s =  a / b
+print("The Division of two value is :", s)
---
+++ python_Basics/Task/task_6.py
@@ -0,0 +1,4 @@
+# 6. Write a Program in python that can input from user greeting message and then display
+greet = str(input("Enter the Greeting message :"))
+
+print("The Greeting Message is :", greet)

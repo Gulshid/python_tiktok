a63f4488e14700a23785515e02ca10f442946abc
Gulshid Zada <gulshidzada4@gmail.com>
2026-01-11 10:28:28 +0500

    Task 3 and 4 done +++>

---
+++ python_Basics/Task/task_3.py
@@ -0,0 +1,16 @@
+# 3. How to Sub Two Number with and Without Third Variable
+
+# Method 1 
+a = 2
+b = 4
+
+# Method 2
+# x = int(input("Enter the value for x :"))
+# y = int(input("Enter the value for y :"))
+
+# Without thired variable method
+print("The Sub of two value is :", a - b)
+
+# With thired variable method
+z = a - b
+print("The Sub of two value is :", z)
---
+++ python_Basics/Task/task_4.py
@@ -0,0 +1,15 @@
+# 4. How to Multiply Two Number with and Without Third Variable
+# Method 1 
+num_1 = 4
+num_2 = 5
+
+# Method 2
+# val_1 = int(input("Enter the value for val_1 :"))
+# val_2 = int(input("Enter the value for val_2 :"))
+
+# Without thired varible method
+print("The Multiply of two value is :", num_1 * num_2)
+
+# with thired. variable method
+val_3 = num_1 * num_2
+print("The Multiply of two value is :", val_3)

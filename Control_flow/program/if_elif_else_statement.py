992a5a8dddf5bcda2ba7419000cdf0b987c71941
Gulshid Zada <gulshidzada4@gmail.com>
2026-01-18 17:06:41 +0500

    if - elif - else statement done

---
+++ Control_flow/program/if_elif_else_statement.py
@@ -0,0 +1,42 @@
+# if - elif- else Statement
+
+# z = 5
+# if condition1:
+#     statement1
+
+# elif condition2:
+#     statement2
+
+
+# elif condition3:
+#     statement3
+
+
+# elif condition4:
+#     statement4
+    
+    
+# else:
+#     statement5
+
+# Example for "if - elif - else statement"
+num = 5
+if num == 1:
+    print("num is 1")
+    
+elif num == 2:
+    print("num is 2")
+    
+elif num == 3:
+    print("num is 3")
+    
+elif num == 4:
+    print("num is 4")
+    
+elif num == 5:
+    print("num is 5")
+    
+else:
+    print("The num value is neither 1,2,3,4,5")
+    
+    

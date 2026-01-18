762efc465283f572ff70e12b4f18941dfc589e3e
Gulshid Zada <gulshidzada4@gmail.com>
2026-01-18 17:23:52 +0500

    Nested if statement

---
+++ Control_flow/program/nested_if_statement.py
@@ -0,0 +1,22 @@
+# 2. nested if Statement
+
+# h = 23
+# if condition_1:
+#     if conditon_2:
+#         Statement1
+    # else:
+        #   statement2
+    
+# else:
+    # statement4
+    
+# Example for "nested if statement"
+c = 10
+
+if c >= 10:
+    if c > 10:
+        print("*** The value of c is greater than 10 ***")
+    else:
+        print("*** The value of c is equal to 10 ***")
+else:
+    print("*** The value of c is less than 10 ***")

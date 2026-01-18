46c565dff4001b7a7cab23b7cf4a53a25034df1b
Gulshid Zada <gulshidzada4@gmail.com>
2026-01-18 16:55:08 +0500

    if - else statement done

---
+++ Control_flow/program/if_else_statement.py
@@ -0,0 +1,19 @@
+# if - else 
+
+# y = 3
+# if condition:
+#     statement
+    
+    
+# else:
+#     statement
+
+# Example of "if - else statement"
+y = 3
+
+if y > 3:
+    print("*** The value of y is greater than 3 ***")
+    
+else:
+    print("*** The value of y is less than 3 ***")
+    

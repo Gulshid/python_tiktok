4bc4d1293bf332cf564d70379194bbe6c82f9322
Gulshid Zada <gulshidzada4@gmail.com>
2026-01-18 16:47:06 +0500

    If statement done

---
+++ Control_flow/program/if_statement.py
@@ -0,0 +1,21 @@
+# x = 2
+# if condition_1:
+#     statement 1
+
+# if conditon_2:
+#     statement 2
+
+# if conditon_3:
+#     statement 3
+
+# Example of "if Statement "
+x = 2
+
+if x == 2:
+    print("The value of x is Two")
+    
+if x == 1:
+    print('The value of x is One')
+
+if x == 0:
+    print("The value of x is Zero")

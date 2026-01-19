95841ef94eff2803bf985e0e0c9bc1bddd0e750e
Gulshid Zada <gulshidzada4@gmail.com>
2026-01-19 20:35:31 +0500

    nested loop done

---
+++ Control_flow/program/nested_loop.py
@@ -0,0 +1,11 @@
+#4. Nested loop
+# 3 * 2 = 6 time
+# for i in range(3):        # outer loop
+#     for j in range(2):    # inner loop
+#         print("Hello")
+
+# Example for "nested loop"
+
+for a in range(4): # Outer loop
+    for b in range(3): # Inner loop
+        print("Hello")

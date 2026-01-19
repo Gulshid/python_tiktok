ed9ffc81482ea67fc0ae396a74f5db81b879008e
Gulshid Zada <gulshidzada4@gmail.com>
2026-01-19 20:14:00 +0500

    while loop done

---
+++ Control_flow/program/while_loop.py
@@ -0,0 +1,20 @@
+# 3. while loop
+
+# a = starting point
+# while condition: ---> ending point
+    # statement
+    # increment / decrement
+
+# Example for "while loop"
+# print 1 to 20 using while loop
+# print 20 to 1 using while loop
+
+var = 1
+while var <= 20:
+    print(var, end = " ")
+    var = var + 1 # increment
+print("\n") 
+var = 20
+while var >= 1:
+    print(var, end = " ")
+    var = var - 1 # decrement

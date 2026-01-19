76b70daf6bac7032af272723fed6c94c9be279f4
Gulshid Zada <gulshidzada4@gmail.com>
2026-01-19 20:06:38 +0500

    for loop done

---
+++ Control_flow/program/for_loop.py
@@ -0,0 +1,12 @@
+# 3. for loop
+
+
+# for variable in range(start, end, step):
+    # statement
+    
+# Example for "for loop"
+
+# print 1 to 10 number using for loop
+for num in range(1, 11):
+    print(num, end = " ")
+    

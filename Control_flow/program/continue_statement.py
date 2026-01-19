31c4920ea5ffb715fe1a702d9d204bdabfba1de2
Gulshid Zada <gulshidzada4@gmail.com>
2026-01-19 21:11:19 +0500

    continue statement

---
+++ Control_flow/program/continue_statement.py
@@ -0,0 +1,12 @@
+# 5. continue statement in python
+
+# for variable in range():
+#     if condition:
+#         continue
+#     statement
+
+# Example for "Continue statement"
+for y in range(1,11):
+    if y == 8:
+        continue # Skip specific 
+    print(y, end = " ")

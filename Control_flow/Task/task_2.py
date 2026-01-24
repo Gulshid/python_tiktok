588656642a4d5206650c66dd409402f949d70146
Gulshid Zada <gulshidzada4@gmail.com>
2026-01-24 07:44:46 +0500

    Task 2 part 1 done

---
+++ Control_flow/Task/task_2.py
@@ -0,0 +1,18 @@
+# 2. Even & Odd Numbers (2 Methods)
+#1.find the even and odd of any Number using if Statement Only 
+# Method 1
+# num = 9
+
+# Method 2
+num = int(input("Enter the value for num :"))
+if num % 2 == 0: # even logic
+    print(f"The Number {num} is Even Number ")
+
+if num % 2 == 1: # odd logic
+    print(f"The Number {num} is Odd Number ")
+    
+
+print("\n")
+#2. Find even & odd numbers using if condition inside loop
+
+    #3. Find even & odd numbers using list slicing / step / modulus (second method)

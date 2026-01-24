58329bc5771e3f8ced39a84ac48ca62646e60904
Gulshid Zada <gulshidzada4@gmail.com>
2026-01-24 07:35:13 +0500

    Task 1 done

---
+++ Control_flow/Task/task_1.py
@@ -0,0 +1,28 @@
+# 1. Number Printing
+# 1.Print numbers 1 → 10 using a for loop
+print("For loop ****")
+for a in range(1, 11):
+    print(a, end = " ")
+
+
+print("\n")   
+print("WHile loop ****")
+# 2.Print numbers 1 → 10 using a while loop
+b = 1
+while b <= 10:
+    print(b, end = " ")
+    b = b + 1
+    
+print("\n")
+print("For loop ****")
+# 3.Print numbers 10 → 1 using a for loop
+for x in range(10, 0, -1):
+    print(x, end = " ")
+
+print("\n")
+print("While loop ****")
+# 4.Print numbers 10 → 1 using a while loop
+y = 10
+while y >= 1:
+    print(y, end = " ")
+    y = y - 1
--- Control_flow/program/content.py
+++ Control_flow/program/content.py
@@ -1,7 +1,7 @@
     #    *** Chapter 2. Control Flow ***
     
-        # 1. if , if - else ,if - elif- else Statement
-        # 2. nested if Statement
-        # 3. for , while loop 
-        # 4. Nested loop
-        # 5. Break and continue statement
+        # 1. if , if - else ,if - elif- else Statement.
+        # 2. nested if Statement.
+        # 3. for , while loop.
+        # 4. Nested loop.
+        # 5. Break and continue statement.

ccbb36bd6ec61b6b5b8efd19a1b9e8581c1f7ff4
Gulshid Zada <gulshidzada4@gmail.com>
2026-01-25 09:31:38 +0500

    Task 3 part 1 done

---
+++ Control_flow/Task/task_3_a.py
@@ -0,0 +1,17 @@
+# 3. Number Comparison
+
+    #1. Check if a number is greater, less, or equal to zero using if–elif–else
+# Method 1
+# num = 3
+
+# Method 2 
+num = int(input("Enter the value for num :"))
+
+if num < 0:
+    print(f"The Number  {num} is less than Zero ***")
+elif num > 0:
+    print(f"The Number   {num}  is greater than Zero ***")
+else:
+    print(f"The Number {num} is equal To Zero ***")
+    
+
